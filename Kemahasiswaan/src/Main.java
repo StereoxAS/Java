74ced8b25ebc4066ba476242f48b4e13d109aaf2
StereoxAS <stereoxas@gmail.com>
2018-03-23 20:49:09 +0700

    update #1

--- Kemahasiswaan/src/Main.java
+++ Kemahasiswaan/src/Main.java
@@ -4,8 +4,8 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("STIS - Kelas");
+		System.out.println("STIS - Kelas	: \n");
 		Kelas2KS1 kelas = new Kelas2KS1();
-		System.out.println(kelas.getMahasiswa(0).getNamaMahasiswa());
+		kelas.getAllMahasiswa();
 	}
 }
