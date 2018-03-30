c38f118f3226a13effc506b092736c355c619110
StereoxAS <stereoxas@gmail.com>
2018-03-30 16:43:59 +0700

    uploaded Tugas2
    ngisi menu

--- Tugas_2/src/Main.java
+++ Tugas_2/src/Main.java
@@ -6,5 +6,8 @@ public class Main
 		Restaurant dominos = new Restaurant("Domino's Pizza", "Otista", "91875", "Jl. Otto Iskandar Dinata No.151", "www.dominos.co.id");
 		Menu menu = new Menu();
 		menu.generateMenu();
+		
+		System.out.println(menu.getListMenu(1).getMenuName() + " seharga Rp." + menu.getListMenu(1).getMenuPrice());
+		
 	}
 }
