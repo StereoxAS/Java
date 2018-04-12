49ee9fa0d138cc08c6490b40b19c2ebfa3f5d146
syahidibnoe <37874526+syahidibnoe@users.noreply.github.com>
2018-04-12 10:00:15 +0700

    Add files via upload

--- Kemahasiswaan/src/Jadwal2KS2.java
+++ Kemahasiswaan/src/Jadwal2KS2.java
@@ -1,20 +1,20 @@
 
 public class Jadwal2KS2 {
 	//private int total;
-	private PJadwal jadwal[] =new PJadwal[7]; 
+	private JadwalParent jadwal[] =new JadwalParent[7]; 
 	
 	public Jadwal2KS2() {
 		PegawaiDosen dosen = new PegawaiDosen();
-		DaftarMataKuliah matkul = new DaftarMataKuliah();
+		MataKuliahList matkul = new MataKuliahList();
 		
 		//this.total=7;
-		this.jadwal[0]= new PJadwal("Senin ","2","323",matkul.getMatakuliah(3),dosen.getDosen(1));
-		this.jadwal[1]= new PJadwal("Senin ","3","265",matkul.getMatakuliah(2),dosen.getDosen(2));
-		this.jadwal[2]= new PJadwal("Selasa","1","341",matkul.getMatakuliah(4),dosen.getDosen(3));
-		this.jadwal[3]= new PJadwal("Selasa","2","325",matkul.getMatakuliah(6),dosen.getDosen(4));
-		this.jadwal[4]= new PJadwal("Rabu  ","1","342",matkul.getMatakuliah(1),dosen.getDosen(5));
-		this.jadwal[5]= new PJadwal("Rabu  ","2","323",matkul.getMatakuliah(5),dosen.getDosen(6));
-		this.jadwal[6]= new PJadwal("Kamis ","2","242",matkul.getMatakuliah(0),dosen.getDosen(0));
+		this.jadwal[0]= new JadwalParent("Senin ","2","323",matkul.getMatakuliah(3),dosen.getDosen(1));
+		this.jadwal[1]= new JadwalParent("Senin ","3","265",matkul.getMatakuliah(2),dosen.getDosen(2));
+		this.jadwal[2]= new JadwalParent("Selasa","1","341",matkul.getMatakuliah(4),dosen.getDosen(3));
+		this.jadwal[3]= new JadwalParent("Selasa","2","325",matkul.getMatakuliah(6),dosen.getDosen(4));
+		this.jadwal[4]= new JadwalParent("Rabu  ","1","342",matkul.getMatakuliah(1),dosen.getDosen(5));
+		this.jadwal[5]= new JadwalParent("Rabu  ","2","323",matkul.getMatakuliah(5),dosen.getDosen(6));
+		this.jadwal[6]= new JadwalParent("Kamis ","2","242",matkul.getMatakuliah(0),dosen.getDosen(0));
 	}
 	
 	public void getJadwalReguler()
@@ -23,7 +23,7 @@ public class Jadwal2KS2 {
 		System.out.println("\nJadwal Reguler Kelas 2 KS 1 \n");
 		
 		System.out.println("---------------------------------------------------------------------------------------------------------");
-		System.out.println("| NO |\tMata Kuliah\t\t\t| Hari   | Sesi | Ruang\t| \tDosen\t\t\t\t\t|");
+		System.out.println("| NO |\tMata Kuliah\t\t\t| Hari   | Sesi | Ruang\t| \tDosen\t\t\t\t|");
 		System.out.println("---------------------------------------------------------------------------------------------------------");
 		
 		for (int index = 0; index < jadwal.length; index++)
